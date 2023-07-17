9c48cf6325e2f83467cd8b859f9b653e31bbc9ab
valmojr <val_mo_jr@hotmail.com>
2023-07-17 02:14:53 -0300

    initial

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Delete, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Delete()
+  getHelloPlus(): string {
+    return this.appService.getHello() + '!!!!';
+  }
+}
