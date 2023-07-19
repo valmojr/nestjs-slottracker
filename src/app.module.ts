353b764f08a89a496d08eeb3d4deac5ce63379b0
Valmo Trindade <70849974+valmojr@users.noreply.github.com>
2023-07-19 16:23:13 +0000

    update package.json and app.module with entity imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { EventModule } from './event/event.module';
+import { GroupModule } from './group/group.module';
+import { RoleModule } from './role/role.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [EventModule, GroupModule, RoleModule, UserModule],
   controllers: [],
   providers: [],
 })
