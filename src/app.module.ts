1a3403981c5208f8067bac0c87775c8c1fa547eb
Valmo Trindade <70849974+valmojr@users.noreply.github.com>
2023-07-21 18:46:31 +0000

    added scaffolded auth entity for hadnling authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { EventModule } from './event/event.module';
 import { GroupModule } from './group/group.module';
 import { RoleModule } from './role/role.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [EventModule, GroupModule, RoleModule, UserModule],
+  imports: [EventModule, GroupModule, RoleModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
