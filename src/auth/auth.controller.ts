adc4f84dbb12f83ccb6ccf57e96113031ffaa961
Valmo Trindade <70849974+valmojr@users.noreply.github.com>
2023-07-24 22:19:54 +0000

    added callback controller for OAuth2, missing code_verifier

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,57 @@
-import { Controller, Get, UseGuards, Req, Res } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req, Res, Logger } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger('AuthController');
+
   @Get('discord')
   @UseGuards(AuthGuard('discord'))
   async discordLogin() {
-    // This route will trigger the Discord OAuth2 flow.
-    return 'Discord OAuth2 flow';
+    this.logger.log('Someone just started logging in with Discord');
+    return {
+      message: 'Discord Login',
+    };
   }
 
   @Get('discord/callback')
   @UseGuards(AuthGuard('discord'))
-  async discordCallback(@Req() req, @Res() res) {
-    // After successful authentication, the user will be redirected here.
-    // You can process the user data in the request "req.user" and
-    // handle your own logic (e.g., store user info in the session).
+  async discordCallback(@Req() req: Request, @Res() res: Response) {
+    // Get the authorization code from the query parameters.
+    const code = req.query.code as string;
 
-    // Redirect the user to another page after successful login.
-    return res.redirect('/dashboard'); // Change '/dashboard' to your desired URL.
+    // Check if the code exists before proceeding.
+    if (!code) {
+      return res.status(400).json({ error: 'Authorization code is missing.' });
+    }
+
+    try {
+      // Make a POST request to exchange the code for an access token.
+      const data = {
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri:
+          'https://valmojr-congenial-halibut-5pp6jwr67jh7qr4-3000.preview.app.github.dev/discord/callback',
+        scope: 'identify email guilds',
+      };
+
+      const response = await fetch('https://discord.com/api/oauth2/token', {
+        method: 'POST',
+        body: new URLSearchParams(data),
+        headers: {},
+      });
+
+      const accessToken = await response.json();
+
+      this.logger.log(accessToken);
+    } catch (error) {
+      console.error('Error exchanging code for access token:', error.message);
+      return res
+        .status(500)
+        .json({ error: 'Failed to fetch user information' });
+    }
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '@prisma/client';
 
