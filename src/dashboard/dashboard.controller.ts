181586543c85c35e8d5bf116a0ca24df6b40101d
Valmo Trindade <70849974+valmojr@users.noreply.github.com>
2023-07-25 18:56:23 +0000

    removed unused functions from dashboard temp entity

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
-import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
 export class DashboardController {
-  constructor(private readonly dashboardService: DashboardService) {}
-
   @Get()
   async getDashboard() {
     return {
--- src/dashboard/dashboard.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class DashboardService {
-  async getDashboard(): Promise<string> {
-    return 'This action returns a dashboard';
-  }
-}
