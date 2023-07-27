204a4aa16305502316375cdca4ba31235b0a5c25
Valmo Trindade <70849974+valmojr@users.noreply.github.com>
2023-07-27 05:20:27 +0000

    removed session entity CRUD, will be handled by express session

--- src/auth/session/session.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@nestjs/common';
-// import { SessionService } from './session.service';
-
-@Controller('session')
-export class SessionController {
-  // constructor(private readonly sessionService: SessionService) {}
-  // NÃO COLOCAR CRUD DE SESSIONS AINDA POR MOTIVOS DE SEGURANÇA
-}
