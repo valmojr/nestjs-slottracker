028da856e932607387b418c85a26dd34d509df49
Valmo Trindade <70849974+valmojr@users.noreply.github.com>
2023-07-27 04:30:44 +0000

    removed oauth2credential entity, its properties are now optional in user

--- src/auth/oauth2-credential/oauth2-credential.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { Oauth2CredentialService } from './oauth2-credential.service';
-
-@Controller('oauth2-credential')
-export class Oauth2CredentialController {
-  constructor(private readonly oauth2CredentialService: Oauth2CredentialService) {}
-}
